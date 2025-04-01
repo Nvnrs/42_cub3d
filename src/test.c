c89c1da15f147007d6914015452d72693ea25a48
zeradiias <132435888+zeradiias@users.noreply.github.com>
2025-04-01 16:39:18 +0200

    map test

--- src/test.c
+++ src/test.c
@@ -53,83 +53,181 @@ typedef struct s_player
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <MLX42/MLX42.h>
+#include "MLX42/MLX42.h"
+#include "../lib/MYLIB42/mylib42.h"
 
 #define WIDTH 512
 #define HEIGHT 512
+#define CUBE_SIZE 64
 
-static mlx_image_t* image;
+
 
 // -----------------------------------------------------------------------------
 
-int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+// int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+// {
+//     return (r << 24 | g << 16 | b << 8 | a);
+// }
+
+// void ft_randomize(void* param)
+// {
+// 	(void)param;
+// 	for (uint32_t i = 0; i < image->width; ++i)
+// 	{
+// 		for (uint32_t y = 0; y < image->height; ++y)
+// 		{
+// 			uint32_t color = ft_pixel(
+// 				rand() % 0xFF, // R
+// 				rand() % 0xFF, // G
+// 				rand() % 0xFF, // B
+// 				rand() % 0xFF  // A
+// 			);
+// 			mlx_put_pixel(image, i, y, color);
+// 		}
+// 	}
+// }
+
+// void ft_hook(void* param)
+// {
+// 	mlx_t* mlx = param;
+
+// 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+// 		mlx_close_window(mlx);
+// 	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+// 		image->instances[0].y -= 5;
+// 	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+// 		image->instances[0].y += 5;
+// 	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+// 		image->instances[0].x -= 5;
+// 	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+// 		image->instances[0].x += 5;
+// }
+
+// // -----------------------------------------------------------------------------
+
+t_bool	is_wall(int x, int y, char **map, int x_max, int y_max)
 {
-    return (r << 24 | g << 16 | b << 8 | a);
+	int	x_in_map;
+	int	y_in_map;
+
+	x_in_map = x / CUBE_SIZE;
+	y_in_map = y / CUBE_SIZE;
+	if (x_in_map >= x_max || y_in_map >= y_max || map[y_in_map][x_in_map] == '1')
+		return (TRUE);
+	return (FALSE);
 }
 
-void ft_randomize(void* param)
+// char	**map_test(int x_max, int y_max)
+// {
+// 	char **map;
+// 	int x;
+// 	int y;
+
+// 	map = malloc((x_max) * sizeof(char*));
+// 	x = 0;
+// 	//map[x][y]
+// 	while (x < x_max)
+// 	{
+// 		y = 0;
+// 		map[x] = malloc(x_max * sizeof(char));
+// 		while (y < y_max)
+// 		{
+// 			if (x == 0 || x == x_max - 1 || y == 0 || y == y_max - 1)
+// 				map[x][y] = '1';
+// 			else
+// 				map[x][y] = '0';
+// 			if (x == 5 && y == 3)
+// 				map[x][y] = 'P';
+// 			y++;
+// 		}
+// 		x++;
+// 	}
+// 	return (map);
+// }
+
+char	**map_test(int x_max, int y_max)
 {
-	(void)param;
-	for (uint32_t i = 0; i < image->width; ++i)
+	char **map;
+	int x;
+	int y;
+
+	map = malloc((y_max) * sizeof(char*));
+	y = 0;
+	//map[x][y]
+	while (y < y_max)
 	{
-		for (uint32_t y = 0; y < image->height; ++y)
+		x = 0;
+		map[y] = malloc(x_max * sizeof(char));
+		while (x < x_max)
 		{
-			uint32_t color = ft_pixel(
-				rand() % 0xFF, // R
-				rand() % 0xFF, // G
-				rand() % 0xFF, // B
-				rand() % 0xFF  // A
-			);
-			mlx_put_pixel(image, i, y, color);
+			if (x == 0 || x == x_max - 1 || y == 0 || y == y_max - 1)
+				map[y][x] = '1';
+			else
+				map[y][x] = '0';
+			if (x == 5 && y == 3)
+				map[y][x] = 'P';
+			x++;
 		}
+		y++;
 	}
+	return (map);
 }
 
-void ft_hook(void* param)
+void	print_map(char **map, int x_max, int y_max)
 {
-	mlx_t* mlx = param;
+	int	x;
+	int	y;
 
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(mlx);
-	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		image->instances[0].y -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		image->instances[0].y += 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		image->instances[0].x -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		image->instances[0].x += 5;
+	y = 0;
+	while (y < y_max)
+	{
+		x = 0;
+		while (x < x_max)
+		{
+			printf("%c ", map[y][x]);
+			x++;
+		}
+		printf("\n");
+		y++;
+	}
+	
 }
-
-// -----------------------------------------------------------------------------
 
 int32_t main(void)
 {
-	mlx_t* mlx;
-
-	// Gotta error check this stuff
-	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
-	{
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
-	}
-	if (!(image = mlx_new_image(mlx, 128, 128)))
-	{
-		mlx_close_window(mlx);
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
-	}
-	if (mlx_image_to_window(mlx, image, 100, 300) == -1)
-	{
-		mlx_close_window(mlx);
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
-	}
+	t_player	player;
+	char **map;
 	
-	mlx_loop_hook(mlx, ft_randomize, mlx);
-	mlx_loop_hook(mlx, ft_hook, mlx);
+	map = map_test(10, 10);
+	player.x = 5 * CUBE_SIZE - CUBE_SIZE / 2;
+	player.y = 3 * CUBE_SIZE - CUBE_SIZE / 2;
+	printf("player is inside wall: %d\n", is_wall(CUBE_SIZE, CUBE_SIZE, map, 10, 10));
+	// mlx_t* mlx;
 
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
+	// // Gotta error check this stuff
+	// if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
+	// {
+	// 	puts(mlx_strerror(mlx_errno));
+	// 	return(EXIT_FAILURE);
+	// }
+	// if (!(image = mlx_new_image(mlx, 128, 128)))
+	// {
+	// 	mlx_close_window(mlx);
+	// 	puts(mlx_strerror(mlx_errno));
+	// 	return(EXIT_FAILURE);
+	// }
+	// if (mlx_image_to_window(mlx, image, 100, 300) == -1)
+	// {
+	// 	mlx_close_window(mlx);
+	// 	puts(mlx_strerror(mlx_errno));
+	// 	return(EXIT_FAILURE);
+	// }
+	
+	// mlx_loop_hook(mlx, ft_randomize, mlx);
+	// mlx_loop_hook(mlx, ft_hook, mlx);
+
+	// mlx_loop(mlx);
+	// mlx_terminate(mlx);
+	print_map(map, 10, 10);
 	return (EXIT_SUCCESS);
 }
