5b7c482341532f1e17b19bff2ce111db462172f0
zeradiias <132435888+zeradiias@users.noreply.github.com>
2025-04-21 09:34:16 +0200

    wall.c clean

--- src/raycasting/wall.c
+++ src/raycasting/wall.c
@@ -6,7 +6,7 @@
 /*   By: pchateau <pchateau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 15:43:02 by pchateau          #+#    #+#             */
-/*   Updated: 2025/04/19 13:29:12 by pchateau         ###   ########.fr       */
+/*   Updated: 2025/04/21 09:34:00 by pchateau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ void	find_wall_hitpoint(t_ray *ray, t_player player)
 	else
 		ray->wall_x = player.x + ray->perp_wall_dist * ray->dir_x;
 	ray->wall_x -= floor(ray->wall_x);
-	// printf("wall_x: %f\n", ray->wall_x);
 }
 
 void	find_texture_x(t_ray *ray, t_map map)
@@ -35,9 +34,6 @@ void	find_texture_x(t_ray *ray, t_map map)
 	else if (ray->side == WEST)
 		texture = map.textures.west;
 	ray->tex_x = ray->wall_x * texture.width;
-	// if (ray->side == EAST || ray->side == SOUTH)//faire attention ici//utile ?
-	// 	ray->tex_x = texture.width - ray->tex_x - 1;
-	// printf("tex_x: %d\n", ray->tex_x);
 }
 
 void	calculate_perpendicular_wall_dist(t_ray *ray)
@@ -57,16 +53,12 @@ void	draw_wall(t_ray *ray, t_images *images, int x, t_map map)
 	ray->draw_end = ray->line_height / 2 + SCREEN_HEIGHT / 2;
 	if (ray->draw_end >= SCREEN_HEIGHT)
 		ray->draw_end = SCREEN_HEIGHT - 1;
-	if (ray->side == NORTH)//NORTH -> BLUE
+	if (ray->side == NORTH)
 		draw_vertical_line_texture(images->wall, ray, x, map.textures.north);
-	else if (ray->side == SOUTH)//SOUTH -> GREEN
+	else if (ray->side == SOUTH)
 		draw_vertical_line_texture(images->wall, ray, x, map.textures.south);
-	else if (ray->side == EAST)//EAST -> RED
+	else if (ray->side == EAST)
 		draw_vertical_line_texture(images->wall, ray, x, map.textures.east);
-	else if (ray->side == WEST)//WEST -> PURPLE
+	else if (ray->side == WEST)
 		draw_vertical_line_texture(images->wall, ray, x, map.textures.west);
-		// draw_vertical_line(images->wall, ray->draw_start, ray->draw_end, x, 0x0000FFFF);
-		// draw_vertical_line(images->wall, ray->draw_start, ray->draw_end, x, 0x00FF00FF);
-		// draw_vertical_line(images->wall, ray->draw_start, ray->draw_end, x, 0xFF0000FF);
-		// draw_vertical_line(images->wall, ray->draw_start, ray->draw_end, x, 0x800080FF);
 }
